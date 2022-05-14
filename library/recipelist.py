4ed89028d46d2e1ddb05f7786a09ed229d40e4d7
Stefan Lichtenstein <lichte13@ads.uni-passau.de>
2022-05-14 14:43:32 +0200

    Vegetarian and Omnivor added as well as more dialogue when adding.

--- library/recipelist.py
+++ library/recipelist.py
@@ -4,28 +4,28 @@ import csv
 
 dishes = []
 
-def load_dishfile() :
+def load_dishfile(category) :
     dishes.clear()
-    with open('library/dishes.csv', newline='') as csvfile:
+    with open('library/dishlibrary/' + category + '_dishes.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar = '|')
         for row in reader:
             dishes.append(row)
 
-def amount_dishes():
+def amount_dishes(category):
     i = 0
-    with open('library/dishes.csv', newline='') as csvfile:
+    with open('library/dishlibrary/' + category + '_dishes.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar = '|')
         for row in reader:
             i += 1
     return i
 
-def get_dish(position):
-    load_dishfile()
+def get_dish(position, filename):
+    load_dishfile(filename)
     return dishes[position]
 
-def add_dish(toAdd):
+def add_dish(toAdd, category):
     dish = toAdd.split(',')
-    with open('library/dishes.csv', 'a+', newline='') as writeobj:
+    with open('library/dishlibrary/' + category + '_dishes.csv', 'a+', newline='') as writeobj:
         csv_writer = csv.writer(writeobj)
         csv_writer.writerow(dish)
         writeobj.close()
