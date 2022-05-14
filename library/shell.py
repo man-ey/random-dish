0fee54c0ad2f4c19ae3d15ba81690a6b713df611
Stefan Lichtenstein <lichte13@ads.uni-passau.de>
2022-05-14 14:43:48 +0200

    Minor shell tweaks

--- library/shell.py
+++ library/shell.py
@@ -6,9 +6,6 @@ import secrets
 dish = ''
 available_commands = ['help', 'vegan', 'details', 'ingredients', 'add', 'quit']
 
-def get_vegan(generated):
-    return get_dish(generated)
-
 def get_ingredients():
     return dish[1:len(dish)]
 
@@ -25,15 +22,20 @@ while 1:
         else:
             print(get_ingredients())
 
-    elif x == 'vegan':
-        dish = get_vegan(secrets.randbelow(amount_dishes()))
+    elif x == 'omnivor' or x == 'vegan' or x == 'vegetarian':
+        dish = get_dish(secrets.randbelow(amount_dishes(x)), x)
         print(dish[0])
     
     elif x == 'add':
         print('List the dish to add in following format, separatet by commas:')
         print('Name, Ingredient 1, Ingredient 2, ..., Ingredient N')
         y = input('> ')
-        add_dish(y)
+        print('Vegan, vegetarian or omnivor dish?')
+        z = input('> ')
+        if z == 'vegan' or z == 'vegetarian' or z == 'omnivor':
+            add_dish(y, z)
+        else:
+            print('Category not available. Try again.')
 
     elif x == 'help':
         print(available_commands)
